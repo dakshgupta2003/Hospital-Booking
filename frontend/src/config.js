c15d299bfedeafe974c074933e5ab969458a0202
dakshgupta2003 <123862062+dakshgupta2003@users.noreply.github.com>
2024-07-04 20:52:40 +0530

    Updated config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // api url
-export const BASE_URL = 'http://localhost:5000/api/v1'; 
+export const BASE_URL = 'https://doctor-backend-450a.onrender.com/api/v1'; 
 export const token = localStorage.getItem("token")
